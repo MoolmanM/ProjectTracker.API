61643ebbe7f73dbcf58b555b46159ecf6135706f
MoolmanM <mmoolman067@gmail.com>
2025-10-13 23:10:41 +0200

    filter: add TODO comment and fix spacing in User model

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,11 @@
-﻿namespace TaskManager.Models
+﻿// TODO
+namespace TaskManager.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public ICollection<Project> Projects {get;set;}
+        public ICollection<Project> Projects { get; set; }
     }
 }
