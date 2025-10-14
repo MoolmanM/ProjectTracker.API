09104599a69192383062c99324057101a39bfe12
MoolmanM <mmoolman067@gmail.com>
2025-10-14 23:05:11 +0200

    Add ApplicationUser model extending IdentityUser

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using NuGet.Common;
+
+namespace TaskManager.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+    }
+}
