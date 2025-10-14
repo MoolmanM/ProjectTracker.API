b4e202e090e6af22ecedd0740e86b17cf8e8fc01
MoolmanM <mmoolman067@gmail.com>
2025-10-14 23:06:01 +0200

    Update AppDbContext to inherit from IdentityDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 using TaskManager.Models;
 
 namespace TaskManager.Data
 {
-    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Models.Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable("Users");
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable("Roles");
+            });
+        }
     }
 }
